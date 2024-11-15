88a984885525dcde5e2fba763365116bba609990
natakurage <uzogamuzol@gmail.com>
2024-11-16 00:25:03 +0900

    とりあえずBasic認証

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const username = process.env.BASIC_AUTH_USERNAME;
+  const password = process.env.BASIC_AUTH_PASSWORD;
+
+  const authHeader = request.headers.get('authorization');
+  if (!authHeader) {
+    return new NextResponse('Unauthorized', {
+      status: 401,
+      headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
+    });
+  }
+
+  const [authType, encoded] = authHeader.split(' ');
+  if (authType !== 'Basic' || !encoded) {
+    return new NextResponse('Unauthorized', {
+      status: 401,
+      headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
+    });
+  }
+
+  const decoded = atob(encoded);
+  const [inputUsername, inputPassword] = decoded.split(':');
+
+  if (inputUsername !== username || inputPassword !== password) {
+    return new NextResponse('Unauthorized', {
+      status: 401,
+      headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
+    });
+  }
+
+  return NextResponse.next();
+}
