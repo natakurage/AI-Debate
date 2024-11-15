d4a62f4f4aaa0a767a85e1eaba3df2db482cb74e
natakurage <uzogamuzol@gmail.com>
2024-11-16 00:30:42 +0900

    contact URL追加

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,11 @@ export function middleware(request: NextRequest) {
   const username = process.env.BASIC_AUTH_USERNAME;
   const password = process.env.BASIC_AUTH_PASSWORD;
 
+  const contact = process.env.CONTACT_URL;
+
   const authHeader = request.headers.get('authorization');
   if (!authHeader) {
-    return new NextResponse('Unauthorized', {
+    return new NextResponse(`Unauthorized (Please contact ${contact})`, {
       status: 401,
       headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
     });
@@ -15,7 +17,7 @@ export function middleware(request: NextRequest) {
 
   const [authType, encoded] = authHeader.split(' ');
   if (authType !== 'Basic' || !encoded) {
-    return new NextResponse('Unauthorized', {
+    return new NextResponse(`Unauthorized (Please contact ${contact})`, {
       status: 401,
       headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
     });
@@ -25,7 +27,7 @@ export function middleware(request: NextRequest) {
   const [inputUsername, inputPassword] = decoded.split(':');
 
   if (inputUsername !== username || inputPassword !== password) {
-    return new NextResponse('Unauthorized', {
+    return new NextResponse(`Unauthorized (Please contact ${contact})`, {
       status: 401,
       headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
     });
