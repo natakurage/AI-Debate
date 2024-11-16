e14b68b695f0476fcc14cf79bb9aed278fb1ec80
natakurage <uzogamuzol@gmail.com>
2024-11-16 14:26:33 +0900

    Revert "また修正"
    This reverts commit dd01d878696640fee785f737b73f419551aa6706.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
     console.log("Judgement started");
     for (let i = 0; i < numJudges; i++) {
       const newJudgment = await judge(agenda, i, history);
-      setJudgments(judgments => [...judgments, newJudgment]);
+      setJudgments([...judgments, newJudgment]);
     }
     const decision = await aggregateJudgments(judgments);
     setDecision(decision);
